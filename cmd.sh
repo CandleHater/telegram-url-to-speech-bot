9ff468498bb884d9df994d2714208f9143e76263
CandleHater <candlehater@bitcoin-node.org>
2020-05-01 00:11:08 +0200

    finalized

--- cmd.sh
+++ cmd.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 echo "# update source"
-# git reset --hard HEAD~1
 git pull
 chmod +x cmd.sh
+echo
 
+echo "# update dependencies"
 npm install --only=production
 echo
 
 echo "# run app"
 export GOOGLE_APPLICATION_CREDENTIALS="/google-keyfile.json"
 node src/app.js
+echo
